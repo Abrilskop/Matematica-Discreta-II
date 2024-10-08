6faa6beb15203c55879687b0a94bbf47c928edbf
Abrilskop <uberpot.ia@gmail.com>
2024-10-07 23:23:28 -0500

    chore: adicion de not

--- MT2-U2-GUIA1-Algebra-Boogle/main.py
+++ MT2-U2-GUIA1-Algebra-Boogle/main.py
@@ -8,3 +8,9 @@ for A in [True, False]:
 for A in [True, False]:
  for B in [True, False]:
   print(A," OR ", B," = ", A or B)
+
+# NOT
+# tenemos un bucle que repetira su proceso 2 veces en una a=true
+for A in [True, False]:
+ for B in [True, False]:
+  print(A," NOT ", B," = ", not B)
