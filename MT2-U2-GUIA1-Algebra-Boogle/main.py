8984217a6da3f6e9bf7ea010cc66043c7995abf2
Abrilskop <116703319+Abrilskop@users.noreply.github.com>
2024-10-20 13:49:41 -0500

    feat: añadir ejercicio 4 a main.py

--- MT2-U2-GUIA1-Algebra-Boogle/main.py
+++ MT2-U2-GUIA1-Algebra-Boogle/main.py
@@ -58,3 +58,31 @@ def imprimir_tabla_verdad(tabla_verdad, variables):
 
 # Llamamos a la función para imprimir la tabla de verdad con los resultados
 imprimir_tabla_verdad(L, ['X','Y','Z','¬Y','¬Z','Y∧¬Z','(Z∨(Y∧¬Z))','(¬Y∧(Z∨(Y∧¬Z)))','(X∧(¬Y∧(Z∨(Y∧¬Z))))','(X∧(¬Y∧(Z∨(Y∧¬Z))))∨¬Z'])
+
+# Ejercicio 4: 
+tabla_verdad = []
+for A in [0, 1]:
+    for B in [0, 1]:
+        for C in [0, 1]:
+            for D in [0, 1]:
+              tabla_verdad.append((A, B, C, D,
+                                   not (A or B), 
+                                   not (C or D),
+                                   (not (A or B) and not (C or D)),
+                                   (not A and not B and not C and not D)))
+
+def imprimir_tabla_verdad(tabla_verdad, variables):
+    encabezado = variables + ["-(A+B)", "-(C+D)", "-(A+B)-(C+D)","-A-B*-C*-D"]
+    separador = "+" + "-" * ((len(encabezado) * 23) - 1) + "+"
+    print(separador)
+    print("|", end="")
+    for enc in encabezado:
+        print(f"{enc.center(22)}|", end="")
+    print("\n" + separador)
+    for fila in tabla_verdad:
+        print("|", end="")
+        for valor in fila:
+            print(f"{str(int(valor)).center(22)}|", end="")
+        print("\n" + separador)
+
+imprimir_tabla_verdad(tabla_verdad, ['A', 'B', 'C', 'D'])
